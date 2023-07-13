6cae46a503c14deda78e08080eee1c83e268f6ad
Arthur Coelho <arthur.cavalcante.puc@gmail.com>
2023-07-13 17:34:46 -0300

    plugin: add telescope

---
+++ README.md
@@ -0,0 +1,2 @@
+# Prerequisites
+install ripgrep
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,48 +4,53 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
-	use {
-		'Mofiqul/dracula.nvim',
-		config = function()
-			vim.cmd('colorscheme dracula')
-		end
-	}
-	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v2.x',
-		requires = {
-			-- LSP Support
-			{ 'neovim/nvim-lspconfig' }, -- Required
-			{       -- Optional
-				'williamboman/mason.nvim',
-				run = function()
-					pcall(vim.cmd, 'MasonUpdate')
-				end,
-			},
-			{ 'williamboman/mason-lspconfig.nvim' }, -- Optional
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+    use {
+        'Mofiqul/dracula.nvim',
+        config = function()
+            vim.cmd('colorscheme dracula')
+        end
+    }
+    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' }, -- Required
+            {                   -- Optional
+                'williamboman/mason.nvim',
+                run = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end,
+            },
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-			-- Autocompletion
-			{ 'hrsh7th/nvim-cmp' }, -- Required
-			{ 'hrsh7th/cmp-nvim-lsp' }, -- Required
-			{ 'L3MON4D3/LuaSnip' }, -- Required
-		}
-	}
-	use {
-		'nvim-tree/nvim-tree.lua',
-		requires = {
-			'nvim-tree/nvim-web-devicons', -- optional
-		},
-		config = function()
-			require("nvim-web-devicons").setup()
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' }, -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'L3MON4D3/LuaSnip' }, -- Required
+        }
+    }
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons', -- optional
+        },
+        config = function()
+            require("nvim-web-devicons").setup()
 
-			require("nvim-tree").setup {
-				view = {
-					width = 20
-				}
-			}
-		end
-	}
+            require("nvim-tree").setup {
+                view = {
+                    width = 20
+                }
+            }
+        end
+    }
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.2',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 end)
